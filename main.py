de50a8329d8d0722bb33ea01c63bd653547e985d
Lucien Donnarieix <lucien.donnarieix@hesias.fr>
2024-01-10 18:53:57 +0100

    Ajout du fichier main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("Choplifter")
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        
+    pygame.display.flip()
+
+pygame.quit()
