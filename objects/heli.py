4c7ac30c0062cb7f5bd80cb5668270fd91325856
Lucien Donnarieix <lucien.donnarieix@hesias.fr>
2024-02-21 17:05:26 +0100

    Modification des attributs du joueurs pour une meilleure compatibilité des mouvements

--- objects/heli.py
+++ objects/heli.py
@@ -4,11 +4,6 @@ class Heli:
     def __init__(self) -> None:
         self.__image = pygame.image.load("assets/imgs/Helicopter_tmp_32x13.png")
         self.__rect = self.__image.get_rect()
-
-        self.__resistance = 0
-        self.__acceleration = 0
-        self.__max_speed = 0
-        self.__velocity = 0
     
     # Geter / Seter
     def get_image(self) -> pygame.Surface:
@@ -20,25 +15,5 @@ class Heli:
         return self.__rect
     def set_rect(self, rect: pygame.Rect) -> None:
         self.__rect = rect
-    
-    def get_resistance(self) -> float:
-        return self.__resistance
-    def set_resistance(self, resistance: float) -> None:
-        self.__resistance = resistance
-    
-    def get_acceleration(self) -> float:
-        return self.__acceleration
-    def set_acceleration(self, acceleration: float) -> None:
-        self.__acceleration = acceleration
-    
-    def get_max_speed(self) -> float:
-        return self.__max_speed
-    def set_max_speed(self, max_speed: float) -> None:
-        self.__max_speed = max_speed
-    
-    def get_velocity(self) -> float:
-        return self.__velocity
-    def set_velocity(self, velocity: float) -> None:
-        self.__velocity = velocity
     
     # Méthodes
--- objects/player.py
+++ objects/player.py
@@ -4,12 +4,37 @@ import objects.heli as heli
 class Player:
     def __init__(self):
         self.__heli = heli.Heli()
+        
+        self.__resistance = 0
+        self.__acceleration = 0
+        self.__max_speed = 0
+        self.__velocity = 0
     
     # Geter / Seter
     def get_heli(self) -> heli.Heli:
         return self.__heli
     def set_heli(self, heli: heli.Heli) -> None:
         self.__heli = heli
+    
+    def get_resistance(self) -> float:
+        return self.__resistance
+    def set_resistance(self, resistance: float) -> None:
+        self.__resistance = resistance
+    
+    def get_acceleration(self) -> float:
+        return self.__acceleration
+    def set_acceleration(self, acceleration: float) -> None:
+        self.__acceleration = acceleration
+    
+    def get_max_speed(self) -> float:
+        return self.__max_speed
+    def set_max_speed(self, max_speed: float) -> None:
+        self.__max_speed = max_speed
+    
+    def get_velocity(self) -> float:
+        return self.__velocity
+    def set_velocity(self, velocity: float) -> None:
+        self.__velocity = velocity
     
     # Méthodes
     def afficher(self, screen: pygame.Surface) -> None:
