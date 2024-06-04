b6315280e8f7fa896d3a1d7916240218b282fe61
RokasS <rokassd9@gmail.com>
2024-06-05 00:07:14 +0200

    Bug SFX reglage dans le Menu Pause

--- objects/menu/menu_son.py
+++ objects/menu/menu_son.py
@@ -44,6 +44,7 @@ class MenuSon:
             
     def update_sound(self, value):
         self.assets.click_sound.set_volume(value)
+        self.assets.up_click_sound.set_volume(value)
         print(f"Son volume: {value}")
 
     def update_music(self, value):
