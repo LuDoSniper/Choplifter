fc14cb043f321790737fa5ffbef19ea0436f47a9
RokasS <rokassd9@gmail.com>
2024-06-06 19:01:19 +0200

    Maps update, Mission et rectification hauteur

---
+++ assets/tilesets/Forest/main.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="main" tilewidth="32" tileheight="32" tilecount="20" columns="10">
+ <image source="../Maps/forest/main.png" width="320" height="64"/>
+</tileset>
