c4937b45d74b6957564003dd4eee468e3918b8ce
RokasS <rokassd9@gmail.com>
2024-06-07 03:58:55 +0200

    Fullscreen mission update

--- objects/menu/menu_mission.py
+++ objects/menu/menu_mission.py
@@ -12,21 +12,29 @@ class MenuMission:
         self.buttons = []
         self.quit_button = None
         self.clicked_element = None
-        self.circle_positions = [(501, 318), (697, 278), (538, 210), (680, 112)]
+        self.circle_positions = [(70, 220), (265, 177), (107, 110), (250, 15)]
         self.create_buttons()
+        self.spacing = 10
 
     def draw(self):
-        dimension = 350
-        map_image = pygame.transform.scale(self.assets.map, (dimension, dimension))
-        map_rect = map_image.get_rect(topright=(self.assets.SCREEN_WIDTH - 20, (self.assets.SCREEN_HEIGHT - self.assets.background_menu_mission.get_height()) // 2 + 20))
+        map_dimension = 350
+        bg_x = (self.assets.SCREEN_WIDTH - self.assets.background_menu_mission.get_width()) // 2
+        bg_y = (self.assets.SCREEN_HEIGHT - self.assets.background_menu_mission.get_height()) // 2
+
+        map_x = bg_x + (self.assets.background_menu_mission.get_width() // 2) + self.spacing
+        map_y = (self.assets.SCREEN_HEIGHT - map_dimension) // 2
+
+        map_image = pygame.transform.scale(self.assets.map, (map_dimension, map_dimension))
+        map_rect = map_image.get_rect(topleft=(map_x, map_y))
+        
         self.screen.blit(map_image, map_rect)
 
-        self.screen.blit(self.assets.background_menu_mission, (20, (self.assets.SCREEN_HEIGHT - self.assets.background_menu_mission.get_height()) // 2 - 20))
+        self.screen.blit(self.assets.background_menu_mission, (bg_x - (self.assets.background_menu_mission.get_width() // 2) - self.spacing, bg_y))
         
         monde_text = self.assets.get_custom_font(16).render(self.monde, True, self.assets.GRIS_FONCE)
-        monde_text_rect = monde_text.get_rect(center=(self.assets.background_menu_mission.get_width() // 2 + 20, 160))
+        monde_text_rect = monde_text.get_rect(center=(bg_x - self.spacing // 2, bg_y + self.assets.background_menu_mission.get_height() // 5))
         self.screen.blit(monde_text, monde_text_rect)
-        
+
         mouse_pos = pygame.mouse.get_pos()
         for button in self.buttons:
             button.update(mouse_pos)
@@ -44,8 +52,8 @@ class MenuMission:
                 color = (0, 0, 0, 0)
             
             pygame.draw.circle(circle_surface, color, (20, 20), 12)
-            self.screen.blit(circle_surface, (self.circle_positions[idx][0] - 20, self.circle_positions[idx][1] - 20))
-    
+            self.screen.blit(circle_surface, (map_x + self.circle_positions[idx][0] - 20, map_y + self.circle_positions[idx][1] - 20))
+
     def create_buttons(self):
         monde_missions = self.missions[self.monde]
         
@@ -53,9 +61,11 @@ class MenuMission:
         original_button_height = self.assets.bouton_unlock.get_height()
         button_width = original_button_width // 1.25
         button_height = original_button_height // 1.25
-        spacing = 15
-        y_start = 180 
-        x_center = self.assets.background_menu_mission.get_width() // 2 + 20
+
+        spacing_buttons = 15 
+        button_x = (((self.assets.SCREEN_WIDTH - self.assets.background_menu_mission.get_width()) // 2) - self.assets.background_menu_mission.get_width() // 2) + ((self.assets.background_menu_mission.get_width() - button_width) // 2) - 10
+        y_start = (self.assets.SCREEN_HEIGHT - (len(monde_missions) * (button_height + spacing_buttons)) + spacing_buttons) // 2 + 50
+        x_center = self.assets.SCREEN_WIDTH // 2
         text_offset_x = 80
 
         self.buttons = []
@@ -66,18 +76,22 @@ class MenuMission:
                 action = lambda m=i+1: self.start_mission(m)
             else:
                 action = None
-            # action = lambda m=i+1: self.start_mission(m) if mission_unlocked else None
 
-            button = Button(f'Mission {i + 1}', x_center - button_width // 2, y_start + i * (button_height + spacing), button_image, button_image_click, action, self.assets, (255,255,255), 20, text_offset_x)
+            button_y = y_start + i * (button_height + spacing_buttons)
+            button = Button(f'Mission {i + 1}', button_x, button_y, button_image, button_image_click, action, self.assets, (255, 255, 255), 20, text_offset_x)
+            
             if not mission_unlocked:
                 self.apply_lock_filter(button.image, button_width, button_height)
-                button.on_click = None 
+                button.on_click = None
+
             self.buttons.append(button)
 
         quit_button_image = pygame.transform.scale(self.assets.bouton, (button_width, button_height))
         quit_button_image_click = pygame.transform.scale(self.assets.bouton_click, (button_width, button_height))
-        self.quit_button = Button('Retour', (self.assets.SCREEN_WIDTH - button_width) // 2 , self.assets.SCREEN_HEIGHT - button_height - 20, quit_button_image, quit_button_image_click, lambda: self.change_menu_callback("play"), self.assets, (255,255,255), 20)
-    
+        quit_button_x = x_center - button_width // 2
+        quit_button_y = y_start + len(monde_missions) * (button_height + spacing_buttons) + 20
+        self.quit_button = Button('Retour', quit_button_x, quit_button_y, quit_button_image, quit_button_image_click, lambda: self.change_menu_callback("play"), self.assets, (255, 255, 255), 20)
+
     def apply_lock_filter(self, surface, button_width, button_height):
         lock_image = pygame.transform.scale(self.assets.bouton_lock, (button_width, button_height))
         surface.blit(lock_image, (0, 0))
@@ -86,6 +100,10 @@ class MenuMission:
         return hasattr(self, var_name)
 
     def handle_event(self, event):
+        map_dimension = 350
+        bg_x = (self.assets.SCREEN_WIDTH - self.assets.background_menu_mission.get_width()) // 2
+        map_x = bg_x + (self.assets.background_menu_mission.get_width() // 2) + self.spacing
+        map_y = (self.assets.SCREEN_HEIGHT - map_dimension) // 2
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             for button in self.buttons:
                 if button.is_hovered(event.pos) and button.on_click is not None:
@@ -112,7 +130,7 @@ class MenuMission:
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             for idx, (monde, _) in enumerate(list(self.missions.items())):
                 position = self.circle_positions[idx]
-                if pygame.Rect(position[0] - 20, position[1] - 20, 40, 40).collidepoint(event.pos):
+                if pygame.Rect(map_x + position[0] - 20, map_y + position[1] - 20, 40, 40).collidepoint(event.pos):
                     self.monde = monde
                     self.create_buttons()
                     self.assets.up_click_sound.play()
@@ -121,7 +139,6 @@ class MenuMission:
     def start_mission(self, mission_number):
         print(f"Mission {mission_number} dans le {self.monde} démarrée")
         return f"{self.monde.split('-')[0]}-{mission_number}"
-        # return f"{mission_number}-{self.monde.split(' ')[0]}"
 
     def set_missions(self, data_missions: dict) -> None:
         self.missions = data_missions
